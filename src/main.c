9d3c4f74bb8853613d466663e82a18303e471953
韦永志 <3262954152@qq.com>
2024-07-27 17:00:08 +0800

    init

--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
 # include <stdio.h>
-
+# include <string.h>
 
 
 
 int main(int argc, char** argv){
     // 第一个参数为文件名
-    ++argc;
-    --argv;
+    --argc;
+    ++argv;
 
-    
+    // 解析参数
+    if(argc == 0){
+        printf("no input file!");
+        return;
+    }    
 
-    getchar();
+    printf("hello world");
+    // getchar();
     return 0;
 }
