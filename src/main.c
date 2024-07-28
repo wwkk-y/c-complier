0c619eb0d147a8e3162e098d43f093fa640b3994
韦永志 <3262954152@qq.com>
2024-07-28 20:30:55 +0800

    update

--- src/main.c
+++ src/main.c
@@ -1,20 +1,68 @@
 # include <stdio.h>
 # include <string.h>
 
+# define VERSION "1.0.0"
+
+// 描述用户输入参数
+typedef struct InputArg {
+    char error;
+    char* error_msg;
+    char* source_file;
+    char* target_file;
+} InputArg;
+
+/**
+ * 生成一个结构体
+ * @return 生成的
+ */
+InputArg* New_Input_Arg(){
+    InputArg* input_arg = (InputArg*) malloc (sizeof(InputArg));
+    if(input_arg == NULL){
+        return NULL;
+    }
+
+    input_arg->error = 0;
+    input_arg->error_msg = NULL;
+    input_arg->source_file = NULL;
+    input_arg->target_file = NULL;
+}
+
+
+
+/**
+ * 解析参数
+ * 
+ */
+InputArg analysis(int argc, char** argv){
+
+}
+
+
+void help(){
+    puts("usage: [-c source] [-o target]");
+
+}
+
+
+/**
+ * 初始化
+ */
+void init(){
+    
+}
 
 
 int main(int argc, char** argv){
-    // 第一个参数为文件名
-    --argc;
-    ++argv;
+    init();
 
-    // 解析参数
     if(argc == 0){
-        printf("no input file!");
+        help();
         return;
     }    
 
-    printf("hello world");
-    // getchar();
+    // 解析参数
+
+
+
     return 0;
 }
